7225c5533c636855ff596406be22853062f0f38d
Emanoel Mattos <emanoelmattos13@gmail.com>
2022-01-05 01:08:09 -0300

    finaliza atividades

--- 20.3/activity.sql
+++ 20.3/activity.sql
@@ -72,3 +72,31 @@ SELECT COUNT(*) quantidade_de_pagamentos FROM payment
 WHERE DATE(payment_date)
 BETWEEN '2005-05-01' AND '2005-08-01';
 
+-- ===========================================================================================
+
+USE sakila;
+
+SELECT * FROM payment;
+
+SELECT * FROM payment
+WHERE DATE(payment_date) = '2005-05-25';
+
+SELECT * FROM payment
+WHERE DATE(payment_date) 
+BETWEEN '2005-01-07' AND '2005-08-22';
+
+
+SELECT * FROM rental;
+SELECT DATE(rental_date) AS 'Data', 
+YEAR(rental_date) AS 'Ano', 
+MONTH(rental_date) AS 'Mês', 
+DAY(rental_date) AS 'Dia', 
+HOUR(rental_date) AS 'Hora', 
+MINUTE(rental_date) AS 'Minuto', 
+SECOND(rental_date) AS 'Segundo'
+FROM rental
+WHERE rental_id = 10330;
+
+SELECT * FROM payment
+WHERE DATE(payment_date) = '2005-07-28'
+AND HOUR(payment_date) >= 22;
