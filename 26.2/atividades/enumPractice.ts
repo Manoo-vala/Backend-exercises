3f87b4a281698f175000a8860a321221e5ddf7d4
Emanoel Mattos <emanoelmattos13@gmail.com>
2022-02-18 16:34:52 -0300

    adiciona aliases e enums da atividade

---
+++ 26.2/atividades/enumPractice.ts
@@ -0,0 +1,34 @@
+enum diasDasemana {
+  domingo = 1,
+  segunda = 2,
+  terça = 3,
+  quarta = 4,
+  quinta = 5,
+  sexta = 6,
+  sabado = 7,
+}
+
+enum coresDoArcoiris {
+  vermelha, 
+  laranja, 
+  amarela, 
+  verde, 
+  azul, 
+  azulEscuro,
+  violeta,
+}
+
+enum comands {
+  salvar, 
+  imprimir, 
+  abrir, 
+  visualizar,
+  fechar,
+}
+
+enum directions {
+  Norte, 
+  Leste, 
+  Sul,
+  Oeste, 
+}
