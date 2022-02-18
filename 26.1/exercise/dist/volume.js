d152f0fa7556f4acb6d352d47880bc72ea419e0a
Emanoel Mattos <emanoelmattos13@gmail.com>
2022-02-17 23:12:34 -0300

    adiciona meu primeiro ts solo hahaaay

---
+++ 26.1/exercise/dist/volume.js
@@ -0,0 +1,22 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.exec = exports.convert = void 0;
+const readline_sync_1 = require("readline-sync");
+function convert(valor, unidadeBase, unidadeParaConversao) {
+    const units = ['km³', 'hm³', 'dam³', 'm³', 'dm³', 'cm³', 'mm³'];
+    const indexUnitBase = units.indexOf(unidadeBase);
+    const indexUnitToConversion = units.indexOf(unidadeParaConversao);
+    const exponent = indexUnitToConversion - indexUnitBase;
+    return valor * Math.pow(10, exponent);
+}
+exports.convert = convert;
+;
+function exec() {
+    const value = (0, readline_sync_1.questionInt)('Qual valor que você quer converter ?');
+    const unityBase = (0, readline_sync_1.question)('Qual a unidade que este número pertence ?');
+    const unityToConversion = (0, readline_sync_1.question)('Qual a unidade que deseja converter ?');
+    console.log(convert(value, unityBase, unityToConversion));
+}
+exports.exec = exec;
+;
+exec();
---
+++ 26.1/exercise/mass.ts
@@ -0,0 +1,21 @@
+import { question, questionInt } from 'readline-sync';
+
+export function convert(valor: number, unidadeBase: string, unidadeParaConversao: string): number {
+  const units = ['kg', 'hg', 'dag', 'g', 'dg', 'cg', 'mg'];
+
+  const indexUnitBase = units.indexOf(unidadeBase);
+  const indexUnitToConversion = units.indexOf(unidadeParaConversao);
+  const exponent = indexUnitToConversion - indexUnitBase;
+
+  return valor * Math.pow(10, exponent);
+};
+
+export function exec(): void {
+  const value = questionInt('Qual valor que você quer converter ?');
+  const unityBase = question('Qual a unidade que este número pertence ?');
+  const unityToConversion = question('Qual a unidade que deseja converter ?');
+
+  console.log(convert(value, unityBase, unityToConversion));
+};
+
+exec();
