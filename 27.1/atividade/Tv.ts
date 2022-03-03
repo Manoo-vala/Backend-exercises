e3ae6d334da34139fda78168304be0c80842eceb
Emanoel Mattos <emanoelmattos13@gmail.com>
2022-03-03 15:57:26 -0300

    adiciona segunda atividade do dia 27.1

--- 27.1/atividade/Tv.ts
+++ 27.1/atividade/Tv.ts
@@ -1,24 +1,40 @@
 class TV {
-  brand: string;
-  size: number;
-  resolution: string;
-  connections: string[];
-  connectedTo: string;
+  private _brand: string;
+  private _size: number;
+  private _resolution: string;
+  private _connections: string[];
+  private _connectedTo: string;
 
   constructor(brand: string, size: number, resolution: string, connections: string[]) {
     console.log(`Created TV ${brand} - ${resolution}`);
-    this.brand = brand;
-    this.size = size;
-    this.resolution = resolution;
-    this.connections = connections;
+    this._brand = brand;
+    this._size = size;
+    this._resolution = resolution;
+    this._connections = connections;
   }
   
-  turnOn(connectedTo) {
-    console.log(`${this.brand} - ${this.size} - ${this.resolution} - ${this.connections} - ${connectedTo}.`);
-    
+  turnOn(): void {
+    console.log(`${this._brand} - ${this._size} - ${this._resolution} - ${this._connections}.`);
+  }
+
+  get connectedTo(): string {
+    return this._connectedTo;
+  }
+  
+  set connectedTo(value: string) {
+    if (this._connections.includes(value)) {
+      this._connectedTo = value;
+    } else {
+      console.log('Invalid Connection to this tv!');
+      
+    }
   }
 };
 
 const firstTv = new TV('Massquerain', 62, '6200px', ['HDMI', 'Ethernet']);
 
-firstTv.turnOn('HDMI');
+firstTv.turnOn();
+
+firstTv.connectedTo = 'HDMI';
+
+console.log(`A tv ${firstTv} está conectada via ${firstTv.connectedTo}`);
