9e3322629c5140b0126ccc8180a90d408848a9e3
Emanoel Mattos <emanoelmattos13@gmail.com>
2022-03-08 01:33:13 -0300

    adiciona desafio 3

--- 27.1/exercises/desafio3.ts
+++ 27.1/exercises/desafio3.ts
@@ -1,21 +1,109 @@
-class client {
-  public name: string;
+class Client {
+  private _name:string = String();
 
   constructor (name: string) {
     this.name = name;
   }
-}
 
-class order {
-  public name: string[];
-  public price: number;
-
-  constructor (name: string[], price: number) {
-    this.name = [];
-    this.price = price;
+  get name():string {
+    return this.name;
   }
 
-  public setNewOrder(newOrder: string[]) {
-    this.name.push(...newOrder);
+  set name(name: string) {
+    if (name.length < 3) {
+      throw new Error('Nome curto demaissss');
+    }
+    this._name = name;
+  }
+}
+
+class OrderProduct {
+  private _name:string = String();
+  private _price:number = Number();
+
+  constructor(name:string, price:number) {
+    this.name = name;
+    this.price = price
+  }
+
+  get name():string {
+    return this._name;
+  }
+
+  get price():number {
+    return this._price;
+  }
+  
+  set name(value:string) {
+    if (value.length < 3) {
+      throw new Error('Nome curto demaissss');
+    }
+    this._name = value;
+  }
+
+  set price(value:number) {
+    if(value < 0) throw new Error('De graça, nem injeção na testa.');
+
+    this._price = value;
+  }
+}
+
+class Order {
+  private _client: Client;
+  private _items: OrderProduct[] = [];
+  private _paymentMethod:string;
+  private _discount:number = 0;
+
+  constructor(client: Client, items:OrderProduct[], pay:string, discount:number) {
+    this._client = client;
+    this.items = items;
+    this._paymentMethod = pay;
+    this.discount = discount;
+  }
+
+  get client():Client {
+    return this._client;
+  };
+
+  set client(value:Client) {
+    this._client = value 
+  }
+
+  get items():OrderProduct[] {
+    return this._items;
+  }
+
+  set items(values:OrderProduct[]) {
+    if (values === []) throw new Error('Precisa haver ao menos um pedido');
+    this._items = values;
+  }
+
+  get paymentMethod():string {
+    return this._paymentMethod;
+  }
+
+  set paymentMethod(pay:string) {
+    this._paymentMethod = pay;
+  }
+
+  get discount():number {
+    return this._discount;
+  }
+
+  set discount(disc:number) {
+    if(disc < 0) throw new Error('O desconto precisa ser válido');
+    this._discount = disc;
+  }
+
+  calculateTotal() {
+    return this.items.reduce((prev, curr) => {
+      const totalValue = prev += curr.price;
+
+      return totalValue;
+    }, 0);
+  }
+
+  calculateWithDiscount() {
+    return this.calculateTotal() * (1 - this._discount)
   }
 }
