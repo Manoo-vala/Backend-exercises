e6c357216cbabc6a929f137c856cd3bda6eac4f0
Emanoel Mattos <emanoelmattos13@gmail.com>
2022-05-01 23:42:39 -0300

    feat: implements enum of errors

---
+++ 30.1/src/types/interfaces/ErrorTypes.ts
@@ -0,0 +1,10 @@
+enum ControllerErrors {
+  internal = 'Internal Server Error',
+  notFound = 'Object not found',
+  requiredId = 'Id is required',
+  badRequest = 'Bad request',
+}
+
+export {
+  ControllerErrors,
+};
